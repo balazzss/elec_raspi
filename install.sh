bd4d03a005e1d36b1c29aafbb4d3c19e6d9f7ce1
Balàzs <31547968+balazzss@users.noreply.github.com>
2021-03-24 20:29:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #ajout d'une interface graphique
-whiptail --title "Bienvenue" --msgbox "Bienvenue dans l'installation des outils pour votre machine, cliquez sur OK pour continuier" 8 78
+whiptail --title "Bienvenue" --msgbox "Bienvenue dans l'installation du raspberry pour le cours de systèmes embarqués" 8 78
 
 #Vérifier la connexion à internet, si aucune alors quitte le programme
 network () {
@@ -36,36 +36,48 @@ whiptail --title "Installation des programmes" --msgbox "Installation des progra
         sudo apt install vim -y
         sudo apt install tmux -y
         sudo apt install curl wget -y
+        sudo apt install wireless-tools wpasupplicant -y
+        sudo apt install arduino -y
         sudo timedatectl set-timezone Europe/Brussels
 }
 
 function_mail () {
         wget https://raw.githubusercontent.com/balazzss/elec_raspi/main/mail_auto.py
+        whiptail --title "Mail automatiques" --msgbox "Le fichier python pour la configuration des mails au démarrage est téléchargé (mail_auto.py)" 8 78
 }
 
-function_changeSSHport () {
-SSHPORT=$(whiptail --inputbox "Change SSH port" 8 78  --title "Entrer le numéro du port que vous souhaitez utiliser pour vous connecter en SSH." 3>&1 1>&2 2>&3)
-echo $SSHPORT
-sudo sed -i "s/#Port 22/Port "$SSHPORT"/g" /etc/ssh/sshd_config
+wpa_supplicant () {
+        WIFISSID=$(whiptail --inputbox "SSID" 8 78  --title "Entrer le SSID" 3>&1 1>&2 2>&3)
+        WIFIPASSWD=$(whiptail --inputbox "WIFISSID" 8 78  --title "Entrer le mot de passe" 3>&1 1>&2 2>&3)
+        sudo wpa_passphrase "$WIFISSID" "$WIFIPASSWD" >> /etc/wpa_supplicant/wpa_supplicant.conf
+}
 
-if (whiptail --title "Public Key" --yesno "Voulez-vous utiliser une clé publique pour vous authentifier avec SSH?" 8 78); then
+function_SSH () {
+        SSHPORT=$(whiptail --inputbox "Change SSH port" 8 78  --title "Entrer le numéro du port que vous souhaitez utiliser pour vous connecter en SSH." 3>&1 1>&2 2>&3)
+        echo $SSHPORT
+        sudo sed -i "s/#Port 22/Port "$SSHPORT"/g" /etc/ssh/sshd_config
+        whiptail --title "Création d'une paire clé SSH" --msgbox "Création d'une paire de clé pour SSH. Veuillez cliquer sur entrer jusqu'à la fin de la création." 8 78
+        ssh-keygen -t rsa -b 4096
+        cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+        cp ~/.ssh/id_rsa ~ && mv id_rsa cle_prive_a_mettre_dans_putty
+        #Activation des clés pour SSH
         sudo sed -i 's/#PubkeyAuthentication no/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
-else
-        sudo sed -i 's/#PubkeyAuthentication no/PubkeyAuthentication no/g' /etc/ssh/sshd_config
-fi
-        
+        sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+        #préciser que les clés sont dans le dossier ~/.ssh et qu'il faut tester manuellement la configuration de la clé avant de désactiver le mot de passe.
+ 
 }
 
 
 end () {
-whiptail --title "Configuration completed" --msgbox "La configuration de votre machine est terminé." 8 78
+whiptail --title "Configuration terminé" --msgbox "La configuration du raspberry est terminée :-)." 8 78
 }
 main () {
         network || error "User exited."
         update_upgrade || error "User exited."
         programms_install || error "User exited."
-        function_changeSSHport || error "User exited."
-
+        function_mail || error "User exited."
+        wpa_supplicant || error "User exited."
+        function_SSH || error "User exited."
         end
         clear
 }
