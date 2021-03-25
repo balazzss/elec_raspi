ed7c8b63937eaa76ba5b4a2bcda68c7d1a61cc87
Balàzs <31547968+balazzss@users.noreply.github.com>
2021-03-25 09:53:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ fi
 }
 
 update_upgrade () {
-whiptail --title "Mise à jour des paquets" --msgbox "Mises à jour des paquets." 8 78
+whiptail --title "Mise à jour des paquets" --msgbox "Mises à jour des paquets. Cela peut prendre un certain temps." 8 78
         clear
         if ! { sudo apt update 2>&1 || echo E: update failed; } | grep -q '^[WE]:'; then
             echo :::apt update success
@@ -33,12 +33,36 @@ whiptail --title "Mise à jour des paquets" --msgbox "Mises à jour des paquets 
 
 programms_install () {
 whiptail --title "Installation des programmes" --msgbox "Installation des programmes nécessaire" 8 78
-        sudo apt install vim -y
-        sudo apt install tmux -y
-        sudo apt install curl wget -y
-        sudo apt install wireless-tools wpasupplicant -y
-        sudo apt install arduino -y
-        sudo timedatectl set-timezone Europe/Brussels
+        if ! { sudo apt install vim -y 2>&1 || echo E: update failed; } | grep -q '^[WE]:'; then
+            echo :::vim installation success
+        else
+            echo :::vim installation failure
+        fi
+        if ! { sudo apt install tmux -y 2>&1 || echo E: update failed; } | grep -q '^[WE]:'; then
+            echo :::tmux installation success
+        else
+            echo :::tmux installation failure
+        fi
+        if ! { sudo apt install curl wget -y 2>&1 || echo E: update failed; } | grep -q '^[WE]:'; then
+            echo :::curl and wget installation success
+        else
+            echo :::curl and wget installation failure
+        fi
+        if ! { sudo apt install wireless-tools wpasupplicant -y 2>&1 || echo E: update failed; } | grep -q '^[WE]:'; then
+            echo :::wireless-tools wpasupplicant installation success
+        else
+            echo :::wireless-tools wpasupplicant installation failure
+        fi
+        if ! { sudo apt install arduino -y 2>&1 || echo E: update failed; } | grep -q '^[WE]:'; then
+            echo :::arduino installation success
+        else
+            echo :::arduino installation failure
+        fi
+        if ! { sudo timedatectl set-timezone Europe/Brussels 2>&1 || echo E: update failed; } | grep -q '^[WE]:'; then
+            echo :::set-timezone Europe/Brussels success
+        else
+            echo :::set-timezone Europe/Brussels failure
+        fi
 }
 
 function_mail () {
